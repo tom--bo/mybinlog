36c8f62d2d7110b3e5a15e1ea3a5e5010d4128ca
tom--bo <otsuka.t.2013@gmail.com>
2019-06-14 00:49:30 +0900

    Add Format Desctiption Event

--- header.go
+++ header.go
@@ -7,28 +7,6 @@ import (
 
 const layout = "2006-01-02 15:04:05"
 
-type Header struct {
-	Timestamp    time.Time
-	Typecode     LogEventType
-	ServerID     int
-	Eventlength  int
-	NextPosition int
-	Flags        []byte
-	ExtraHeader  []byte // Not used for now
-}
-
-func (hd Header) String() string {
-	ret := ""
-	ret = fmt.Sprintf(`%s --------
-  EventType   : %s
-  ServerID    : %d
-  Eventlength : %d
-  NextPosition: %d
-  Flags       : %v
-`, hd.Timestamp.Format(layout), hd.Typecode, hd.ServerID, hd.Eventlength, hd.NextPosition, hd.Flags)
-	return ret
-}
-
 type LogEventType int
 
 const (
@@ -150,3 +128,100 @@ func (lt LogEventType) String() string {
 	}
 	return "UNKNOWN_EVENT"
 }
+
+
+type Event struct {
+	header Header
+	body Ibody // event_data
+}
+
+type Header struct {
+	Timestamp    time.Time
+	Typecode     LogEventType
+	ServerID     int
+	Eventlength  int
+	NextPosition int
+	Flags        []byte
+	ExtraHeader  []byte // Not used for now
+}
+
+func (hd Header) String() string {
+	ret := ""
+	ret = fmt.Sprintf(`%s --------
+  EventType   : %s
+  ServerID    : %d
+  Eventlength : %d
+  NextPosition: %d
+  Flags       : %v
+`, hd.Timestamp.Format(layout), hd.Typecode, hd.ServerID, hd.Eventlength, hd.NextPosition, hd.Flags)
+	return ret
+}
+
+// event_data
+type Ibody interface {
+	PrintBody()
+}
+
+// UNKNOWN_EVENT
+type UnknownEvent struct {
+	id int
+}
+func (ue UnknownEvent) PrintBody() {
+	fmt.Println("UnkownEvent")
+}
+
+// START_EVENT_V3
+// QUERY_EVENT
+type QueryEvent struct {
+	id int
+}
+func (qe QueryEvent) PrintBody() {
+	fmt.Println("QueryEvent")
+}
+
+// STOP_EVENT
+// ROTATE_EVENT
+// INTVAR_EVENT
+// LOAD_EVENT
+// SLAVE_EVENT
+// CREATE_FILE_EVENT
+// APPEND_BLOCK_EVENT
+// EXEC_LOAD_EVENT
+// DELETE_FILE_EVENT
+// NEW_LOAD_EVENT
+// RAND_EVENT
+// USER_VAR_EVENT
+// FORMAT_DESCRIPTION_EVENT
+type FormatDescriptionEvent struct {
+	BinlogEvent int
+	ServerVersion string
+	CreateTimeStamp time.Time
+	HeaderLength int
+	PostHeaderLength []byte
+	// PostHeaderLength []int
+}
+func (fde FormatDescriptionEvent) PrintBody() {
+	fmt.Println("FormatDescriptionEvent")
+}
+
+// XID_EVENT
+// BEGIN_LOAD_QUERY_EVENT
+// EXECUTE_LOAD_QUERY_EVENT
+// TABLE_MAP_EVENT
+// PRE_GA_WRITE_ROWS_EVENT
+// PRE_GA_UPDATE_ROWS_EVENT
+// PRE_GA_DELETE_ROWS_EVENT
+// WRITE_ROWS_EVENT
+// UPDATE_ROWS_EVENT
+// DELETE_ROWS_EVENT
+// INCIDENT_EVENT
+// HEARTBEAT_LOG_EVENT
+// IGNORABLE_LOG_EVENT
+// ROWS_QUERY_LOG_EVENT
+// WRITE_ROWS_EVENT2
+// UPDATE_ROWS_EVENT2
+// DELETE_ROWS_EVENT2
+// GTID_LOG_EVENT
+// ANONYMOUS_GTID_LOG_EVENT
+// PREVIOUS_GTIDS_LOG_EVENT
+// ENUM_END_EVENT
--- main.go
+++ main.go
@@ -8,9 +8,59 @@ import (
 	"time"
 )
 
-func getTimestamp(b []byte) (int, error) {
-	ts := 0
-	return ts, nil
+func parseData(typeCode LogEventType, d []byte) Ibody {
+	switch typeCode {
+	case UNKNOWN_EVENT:
+		return UnknownEvent{}
+	case START_EVENT_V3:
+	case QUERY_EVENT:
+	case STOP_EVENT:
+	case ROTATE_EVENT:
+	case INTVAR_EVENT:
+	case LOAD_EVENT:
+	case SLAVE_EVENT:
+	case CREATE_FILE_EVENT:
+	case APPEND_BLOCK_EVENT:
+	case EXEC_LOAD_EVENT:
+	case DELETE_FILE_EVENT:
+	case NEW_LOAD_EVENT:
+	case RAND_EVENT:
+	case USER_VAR_EVENT:
+	case FORMAT_DESCRIPTION_EVENT: // 15
+		ret := FormatDescriptionEvent{
+			BinlogEvent: int(binary.LittleEndian.Uint16(d[:2])),
+			ServerVersion: string(d[2:52]),
+			CreateTimeStamp: time.Unix(int64(binary.LittleEndian.Uint32(d[52:56])), 0),
+			HeaderLength: int(d[56]),
+			PostHeaderLength: d[57:],
+		}
+		fmt.Println("----")
+		fmt.Println(ret)
+		fmt.Println("----")
+		return ret
+	case XID_EVENT:
+	case BEGIN_LOAD_QUERY_EVENT:
+	case EXECUTE_LOAD_QUERY_EVENT:
+	case TABLE_MAP_EVENT:
+	case PRE_GA_WRITE_ROWS_EVENT:
+	case PRE_GA_UPDATE_ROWS_EVENT:
+	case PRE_GA_DELETE_ROWS_EVENT:
+	case WRITE_ROWS_EVENT:
+	case UPDATE_ROWS_EVENT:
+	case DELETE_ROWS_EVENT:
+	case INCIDENT_EVENT:
+	case HEARTBEAT_LOG_EVENT:
+	case IGNORABLE_LOG_EVENT:
+	case ROWS_QUERY_LOG_EVENT:
+	case WRITE_ROWS_EVENT2:
+	case UPDATE_ROWS_EVENT2:
+	case DELETE_ROWS_EVENT2:
+	case GTID_LOG_EVENT:
+	case ANONYMOUS_GTID_LOG_EVENT:
+	case PREVIOUS_GTIDS_LOG_EVENT:
+	case ENUM_END_EVENT:
+	}
+	return UnknownEvent{}
 }
 
 func main() {
@@ -33,18 +83,23 @@ func main() {
 		if err != nil {
 			fmt.Println("Some error happen during reading files")
 		}
-
-		fmt.Println(string(buf[:4]))
 	}
 
-	pos := 4
 	l := len(buf)
+
+	if l < 4 || string(buf[1:4]) != "bin" {
+		fmt.Println("This is not a binlog!!")
+		return
+	}
+
+	events := []Event{}
+	pos := 4
 	for pos+19 < l-1 && pos != 0 {
-		// ts, err := getTimestamp(buf[pos:pos+4])
-		if err != nil {
-			fmt.Println(err)
+		if int64(binary.LittleEndian.Uint32(buf[pos : pos+4])) == 0 {
+			// remaining bytes
 			break
 		}
+
 		ts := int64(binary.LittleEndian.Uint32(buf[pos : pos+4]))
 		head := Header{
 			Timestamp:    time.Unix(ts, 0),
@@ -65,8 +120,14 @@ func main() {
 		}
 
 		fmt.Println(head)
+		b := parseData(head.Typecode, buf[pos+19:head.NextPosition])
+
+		event := Event {
+			header: head,
+			body: b,
+		}
+		events = append(events, event)
 
 		pos = head.NextPosition
-
 	}
 }
